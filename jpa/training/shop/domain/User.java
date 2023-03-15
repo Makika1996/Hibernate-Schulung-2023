4363c43ed45e9c71279c9292374f7cfdd1c6b091
Marija Marinkov <Marija Marinkov@eiskonzept.ag>
2023-03-15 11:29:19 +0100

    first commit

---
+++ jpa/training/shop/domain/User.java
@@ -0,0 +1,54 @@
+package jpa.training.shop.domain;
+
+import jakarta.persistence.*;
+
+@Table(name = "BENUTZER")
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Benutzer: %s (%s)", username, email);
+    }
+
+}
